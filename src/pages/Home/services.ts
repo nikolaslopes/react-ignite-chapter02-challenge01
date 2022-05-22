e1d3e20878b02e66834c27d86a304082a4664999
Nikolas Lopes <nikolaslopes.dev@gmail.com>
2022-05-22 17:42:21 -0400

    :pencil2: created products interface

--- src/pages/Home/services.ts
+++ src/pages/Home/services.ts
@@ -1,7 +1,7 @@
 import { api } from '../../services/api'
 
-export async function loadProducts() {
-  const request = await api.get('products')
+export async function getProducts() {
+  const { data } = await api.get('products')
 
-  console.log(request)
+  return data
 }
